b5c0224f920f0f7f4cc53f8cd4dfc960c1c01768
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-08 21:50:27 +0000

    Implement Fixed class with constructors, accessors, and assignment operator

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -3,10 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 19:43:38 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/02/20 19:43:39 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/03/06 01:56:53 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+
+const int Fixed::fracBits = 8; 
+
+Fixed::Fixed()
+{
+    this->rawBits = 0;
+}
+Fixed::~Fixed()
+{
+
+}
+Fixed::Fixed(const Fixed & other)
+{
+    *this = other;
+}
+int Fixed::getRawBits( void ) const
+{
+    return (this->rawBits); 
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    this->rawBits = raw;
+}
+
+
+Fixed & Fixed::operator= (const Fixed& other) {
+
+    if (this != &other)
+    {
+        this->rawBits = other.getRawBits();
+    }
+    return (*this);
+}
