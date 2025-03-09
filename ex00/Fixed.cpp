94c5597025620548e66eb19750a754d150dbf4e5
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-09 03:31:49 +0000

    Add debug output to Fixed class constructors and methods

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,40 +6,44 @@
 /*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 19:43:38 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/03/06 01:56:53 by macbookpro       ###   ########.fr       */
+/*   Updated: 2025/03/09 03:31:32 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-
+#include <iostream>
 
 const int Fixed::fracBits = 8; 
 
 Fixed::Fixed()
 {
+    std::cout << "Default Constructor called" << std::endl;
     this->rawBits = 0;
 }
 Fixed::~Fixed()
 {
-
+    std::cout << "Deconstructor called" << std::endl; 
 }
 Fixed::Fixed(const Fixed & other)
 {
+    std::cout << "Copy Constructor called" << std::endl;
     *this = other;
 }
 int Fixed::getRawBits( void ) const
 {
+    std::cout << "getRawBits member function called" << std::endl;
     return (this->rawBits); 
 }
 
 void Fixed::setRawBits( int const raw )
 {
+    std::cout << "setRawBits member function called" << std::endl;
     this->rawBits = raw;
 }
 
-
-Fixed & Fixed::operator= (const Fixed& other) {
-
+Fixed & Fixed::operator= (const Fixed& other) 
+{
+    std::cout << "Assignation operator called" << std::endl;
     if (this != &other)
     {
         this->rawBits = other.getRawBits();
