6bf1b7bf1f12a3dd564b60380fb13dd527aa329b
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-13 22:26:40 +0000

    Add Fixed class header with constructors, destructor, and member functions

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 19:43:36 by ael-moha          #+#    #+#             */
+/*   Updated: 2025/03/13 22:26:24 by macbookpro       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed {
+
+private:
+    int rawBits;
+    static const int fracBits;
+
+public:
+    Fixed(const int nbr);
+    Fixed(const float nbr);
+    ~Fixed();
+
+    // copy constructor
+    Fixed(const Fixed & other);
+    
+    //copy assignment constructor 
+    Fixed & operator=(const Fixed & other);
+    
+    float toFloat( void ) const;
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+};
+
+
+
+#endif
