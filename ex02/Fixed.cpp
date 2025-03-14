0e5a1ba8e75cdc93611861097084865c151c353f
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-14 20:50:03 +0000

    ex02 : Implement Fixed class with comparison operators

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 19:43:38 by ael-moha          #+#    #+#             */
+/*   Updated: 2025/03/14 20:49:01 by macbookpro       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::fracBits = 8; 
+
+
+Fixed::Fixed()
+{
+    std::cout << "Default Constructor called" << std::endl;
+    this->rawBits = 0;
+}
+
+Fixed::Fixed(const int nbr)
+{
+    std::cout << "Int Constructor called" << std::endl;
+    this->rawBits = nbr << Fixed::fracBits;
+}
+
+Fixed::Fixed(const float nbr)
+{
+    std::cout << "Float Constructor called" << std::endl;
+    this->rawBits = static_cast<int>(roundf(nbr * (1 << fracBits)));
+}
+Fixed::~Fixed()
+{
+    std::cout << "Deconstructor called" << std::endl; 
+}
+
+float Fixed::toFloat( void ) const
+{
+    return static_cast<float>(this->rawBits) / (1 << fracBits);
+}
+
+int Fixed::toInt( void ) const
+{
+    return static_cast<int>(this->rawBits >> this->fracBits);  
+}
+
+Fixed::Fixed(const Fixed & other)
+{
+    std::cout << "Copy Constructor called" << std::endl;
+    *this = other;
+}
+int Fixed::getRawBits( void ) const
+{
+    return (this->rawBits); 
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    this->rawBits = raw;
+}
+
+bool Fixed::operator>(const Fixed& other)
+{
+    return (this->rawBits < other.getRawBits());
+}
+bool Fixed::operator<(const Fixed& other)
+{
+    return (this->rawBits < other.getRawBits());
+}
+
+bool Fixed::operator>=(const Fixed& other)
+{
+    return (this->rawBits >= other.getRawBits());
+}
+
+bool Fixed::operator<=(const Fixed& other)
+{
+    return (this->rawBits <= other.getRawBits());
+}
+
+bool Fixed::operator==(const Fixed& other)
+{
+    return (this->rawBits == other.getRawBits());
+}
+
+bool Fixed::operator!=(const Fixed& other)
+{
+    return (this->rawBits != other.getRawBits());
+}
+
+Fixed & Fixed::operator=(const Fixed& other) 
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        this->rawBits = other.getRawBits();
+    }
+    return (*this);
+}
+
+ // Overloading the << operator to insert floating-point representation
+std::ostream& operator<<(std::ostream& os, const Fixed& obj) {
+    os << obj.toFloat();  
+    return os;
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 19:43:36 by ael-moha          #+#    #+#             */
+/*   Updated: 2025/03/14 20:44:13 by macbookpro       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath> 
+
+class Fixed {
+
+private:
+    int rawBits;
+    static const int fracBits;
+
+public:
+    Fixed();
+    Fixed(const int nbr);
+    Fixed(const float nbr);
+    ~Fixed();
+
+    // copy constructor
+    Fixed(const Fixed & other);
+    
+    //copy assignment constructor 
+    Fixed & operator=(const Fixed & other);
+
+    // comparison operators 
+    bool operator>(const Fixed& other);
+    bool operator<(const Fixed& other);
+    bool operator>=(const Fixed& other);
+    bool operator<=(const Fixed& other);
+    bool operator==(const Fixed& other);
+    bool operator!=(const Fixed& other); 
+
+    float toFloat( void ) const;
+    int toInt( void ) const;
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+
+
+
+#endif
