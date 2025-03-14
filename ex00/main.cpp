34fbe50ff4523ae73ebfd2a844c0c2ea0e4a259b
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-14 22:44:54 +0000

    Fix getRawBits function call in main and update author information

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 19:43:34 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/02/20 21:01:40 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/03/14 22:44:48 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "Fixed.hpp"
 #include <iostream>
 
 int main( void ) {
@@ -20,5 +20,7 @@ int main( void ) {
     c = b;
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits << std::endl; 
+    std::cout << c.getRawBits() << std::endl; 
+
+    return (0);
 }
